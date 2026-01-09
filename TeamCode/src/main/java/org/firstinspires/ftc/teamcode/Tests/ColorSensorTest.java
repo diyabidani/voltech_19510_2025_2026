9dcbdee80e618bbba4e2fa0dc123998e8234bfa3
Diya Bidani <diya_bidani@myabcusd.org>
2026-01-08 18:29:35 -0800

    classes and stuff idk

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/ColorSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/ColorSensorTest.java
@@ -1,11 +1,14 @@
 package org.firstinspires.ftc.teamcode.Tests;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 
+@TeleOp(name = "color sensor test")
 public class ColorSensorTest extends OpMode {
 
     public ColorSensor sensor;
+    public int artifactColor;
 
     @Override
     public void init() {
@@ -14,6 +17,16 @@ public class ColorSensorTest extends OpMode {
 
     @Override
     public void loop() {
+        if(sensor.green()>sensor.red()&& sensor.green()>sensor.blue()){
+            telemetry.addData("GREEN", "");
+        } else if(sensor.blue()>sensor.green()&&sensor.blue()>sensor.red()){
+            telemetry.addData("PURPLE", "");
+        }
+        if(sensor.alpha()>50){
+            telemetry.addData("BALL DETECTED", "");
+        }else{
+            telemetry.addData("NO BALL DETECTED", "");
+        }
         telemetry.addData("green: ", sensor.green());
         telemetry.addData("red: ", sensor.red());
         telemetry.addData("blue: ", sensor.blue());
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode.pedroPathing;
+
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.follower.FollowerConstants;
+import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.localization.constants.PinpointConstants;
+import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+public class Constants {
+    public static PinpointConstants localizerConstants = new PinpointConstants()
+            .forwardPodY(0)
+            .strafePodX(0)
+            .distanceUnit(DistanceUnit.INCH)
+            .hardwareMapName("pinpoint")
+            .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
+            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
+            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
+    public static FollowerConstants followerConstants = new FollowerConstants()
+            .mass(10.20583);
+    public static MecanumConstants driveConstants = new MecanumConstants()
+            .maxPower(1)
+            .rightFrontMotorName("fr")
+            .rightRearMotorName("br")
+            .leftRearMotorName("bl")
+            .leftFrontMotorName("fl")
+            .leftFrontMotorDirection(DcMotorEx.Direction.REVERSE)
+            .leftRearMotorDirection(DcMotorEx.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorEx.Direction.FORWARD)
+            .rightRearMotorDirection(DcMotorEx.Direction.FORWARD)
+            .useBrakeModeInTeleOp(true);
+    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+    public static Follower createFollower(HardwareMap hardwareMap) {
+        return new FollowerBuilder(followerConstants, hardwareMap)
+                .pathConstraints(pathConstraints)
+                .pinpointLocalizer(localizerConstants)
+                .mecanumDrivetrain(driveConstants)
+                .build();
+    }
+}
